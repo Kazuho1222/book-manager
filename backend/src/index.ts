2ab44f4f90539c7ed82a77c634042b45dae7a07a
Kazuho1222 <destiny5914@gmail.com>
2025-02-04 08:51:45 +0900

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,57 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+type BookManager = {
+  id: number
+  name: string
+  status: string
+}
+
+const bookManager: BookManager[] = [
+  { id: 1, name: "React入門", status: "在庫あり" },
+  { id: 2, name: "TypeScript入門", status: "貸出中" },
+  { id: 3, name: "Next.js入門", status: "返却済" },
+]
+
+app.get('/books', async (c) => {
+  const query = c.req.query()
+  const keyword = query.keyword
+
+  if (keyword) {
+    return c.json(bookManager.filter((book) => book.name.includes(keyword)))
+  }
+
+  return c.json(bookManager)
+})
+
+app.post('/books', async (c) => {
+  const body = await c.req.json()
+  const name = body.name
+
+  if (name === "") {
+    return c.json({ error: "書籍名は必須です" })
+  }
+
+  const newBook = {
+    id: bookManager.length + 1,
+    name: name,
+    status: "在庫あり",
+  }
+
+  bookManager.push(newBook)
+  return c.json(newBook)
+})
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 8080
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
